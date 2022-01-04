92a59180e09de08f0475b8f76ce907d7458d03b3
Ihor Kozar <i.kozar97@gmail.com>
2022-01-04 13:46:06 +0200

    migrate to combineReducers

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -1,25 +1,24 @@
-import 'package:flutter_redux_app/redux/app_state.dart';
+import 'package:redux/redux.dart';
+
 import 'actionas.dart';
+import 'app_state.dart';
 
 AppState reducer(AppState state, dynamic action) => AppState(
-    counter: _counterReducer(state, action), text: _textReducer(state, action));
+    counter: _counterReducer(state.counter, action),
+    text: _textReducer(state.text, action));
 
-int _counterReducer(AppState state, dynamic action) {
-  if (action is AddAction) {
-    return state.counter + 1;
-  } else if (action is RemoveAction) {
-    return state.counter - 1;
-  } else {
-    return state.counter;
-  }
-}
+Reducer<int> _counterReducer = combineReducers([
+  TypedReducer<int, AddAction>(_addCounterReducer),
+  TypedReducer<int, RemoveAction>(_removeCounterReducer),
+]);
 
-String _textReducer(AppState state, dynamic action) {
-  if (action is SetTextAction) {
-    return action.text;
-  } else if (action is ResetAction) {
-    return '';
-  } else {
-    return state.text;
-  }
-}
+Reducer<String> _textReducer = combineReducers([
+  TypedReducer<String, SetTextAction>(_setTextReducer),
+  TypedReducer<String, ResetAction>(_resetTextReducer),
+]);
+
+String _setTextReducer(String text, SetTextAction action) => action.text;
+String _resetTextReducer(String text, ResetAction action) => '';
+
+int _addCounterReducer(int count, AddAction action) => count + 1;
+int _removeCounterReducer(int count, RemoveAction action) => count - 1;
